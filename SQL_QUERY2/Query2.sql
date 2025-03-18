47a25bc3e5cb60b5798599b14c52a0451e99dec3
sriha <sriha@sriharika>
2025-03-18 11:42:57 -0500

    Sql querys

---
+++ SQL_QUERY2/Query2.sql
@@ -0,0 +1,13 @@
+"""Fetch the top 5 athletes who have won the most gold medals."""
+with t1 as 
+	(select name, count(1) as total_medals
+	from OLYMPICS_HISTORY
+	where medal = 'Gold'
+	group by name
+	order by count (1) desc),
+t2 as
+	(select *, dense_rank() over(order by total_medals desc ) as rnk
+	from t1)
+select *
+from t2
+where rnk <= 5;
